0a9c837ce246f4aa88fc081963d82dcb244e8c10
Pavlova Polina  (PP6) <polina2001.pavlova@mail.ru>
2022-06-14 20:22:49 +0300

    add t09anim

---
+++ T09ANIM/src/pp6.h
@@ -0,0 +1,12 @@
+/* FILE NAME   : pp6.h
+ * PROGRAMMER  : PP6
+ * LAST UPDATE : 09.06.2022
+ * PURPOSE     : 3D animation project.
+ *               Startup module.
+ */
+#ifndef __pp6_h_
+#include "anim/rnd/rnd.h"
+#include "units/units.h"
+#endif /* __pp6_h_ */
+
+
