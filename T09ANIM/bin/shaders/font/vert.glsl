4659699d47f4db8f88fdb502c00eec33b59e82c2
Pavlova Polina  (PP6) <polina2001.pavlova@mail.ru>
2022-06-18 19:08:27 +0300

    add t09anim

---
+++ T09ANIM/bin/shaders/font/vert.glsl
@@ -0,0 +1,27 @@
+/**/
+#version 330
+
+layout(location = 0) in vec3 InPosition;
+layout(location = 1) in vec2 InTexCoord;
+layout(location = 2) in vec3 InNormal;
+layout(location = 3) in vec4 InColor;
+
+uniform mat4 MatrWVP;
+uniform mat4 MatrW;
+uniform mat4 MatrWInv;
+uniform float Time;
+
+out vec3 DrawPos;
+out vec3 DrawNormal;
+out vec2 DrawTexCoord;
+out vec4 DrawColor;
+
+
+void main( void )
+{
+  gl_Position = MatrWVP * vec4(InPosition + vec3(0, 0, 0), 1);
+  DrawPos = (MatrW * vec4(InPosition, 1)).xyz;
+  DrawColor = InColor;
+  DrawNormal = mat3(MatrWInv) * InNormal;
+  DrawTexCoord = InTexCoord;
+}
